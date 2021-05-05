8c29133b196471428da984c4b40eb31e271a7863
Aleksey Belchenkov <u608110@gmail.com>
2021-05-05 16:25:58 +0400

    Added admin panel guard

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.
 Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
 
 Route::prefix('admin')
-    ->middleware(['auth'])
     ->as('admin.')
     ->namespace('Admin')
+    ->middleware(['auth', 'can:admin-panel'])
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('home');
         Route::post('/users/{user}/verify', 'UsersController@verify')->name('users.verify');
