789b8c6f5f87aedcb88e007685be9145aa0607d4
Aleksey Belchenkov <u608110@gmail.com>
2021-05-25 12:47:21 +0400

    Added moderator role

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -29,6 +29,7 @@ class User extends Authenticatable
     public const STATUS_ACTIVE = 'active';
 
     public const ROLE_USER = 'user';
+    public const ROLE_MODERATOR = 'moderator';
     public const ROLE_ADMIN = 'admin';
 
     protected $fillable = [
@@ -56,6 +57,7 @@ class User extends Authenticatable
     {
         return [
             self::ROLE_USER => 'User',
+            self::ROLE_MODERATOR => 'Moderator',
             self::ROLE_ADMIN => 'Admin',
         ];
     }
@@ -203,6 +205,11 @@ class User extends Authenticatable
     public function isAdmin(): bool
     {
         return $this->role === self::ROLE_ADMIN;
+    }
+
+    public function isModerator(): bool
+    {
+        return $this->role === self::ROLE_MODERATOR;
     }
 
     public function isPhoneVerified(): bool
